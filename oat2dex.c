96eaffecd7ef853abe59fbe3a4566d56977956fe
Wu Yongzheng <wuyongzheng@gmail.com>
2014-11-28 15:08:47 +0800

    init

---
+++ oat2dex.c
@@ -0,0 +1,48 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void write_to_file (unsigned char *data, int size, int file_count)
+{
+	char filename[16];
+	sprintf(filename, "dex%02d.dex", file_count);
+	printf("Writing %d bytes to %s\n", size, filename);
+	FILE *fp = fopen(filename, "wb");
+	fwrite(data, 1, size, fp);
+	fclose(fp);
+}
+
+int main (int argc, char *argv[])
+{
+	if (argc != 2) {
+		printf("Usage: %s file.oat\n", argv[0]);
+		return 1;
+	}
+
+	FILE *infp = fopen(argv[1], "rb");
+	if (infp == NULL) {
+		printf("can't open %s\n", argv[1]);
+		return 1;
+	}
+
+	fseek(infp, 0, SEEK_END);
+	int insize = ftell(infp);
+	fseek(infp, 0, SEEK_SET);
+	unsigned char *indata = malloc(insize);
+	fread(indata, 1, insize, infp);
+	fclose(infp);
+
+	int file_count = 0;
+	int start = -1;
+	int ptr;
+	for (ptr = 0; ptr < insize; ptr ++) {
+		if (memcmp(indata+ptr, "dex\n035", 8) != 0)
+			continue;
+		if (start != -1)
+			write_to_file(indata+start, ptr - start, ++file_count);
+		start = ptr;
+	}
+	if (start != -1)
+		write_to_file(indata+start, ptr - start, ++file_count);
+	return 0;
+}
